c85d855a21d81c9609cf4afbaa565568ae72392d
Lois Di Qual <lois@siteline.com>
2020-02-11 18:52:26 -0800

    macOS target

---
+++ Introspect macOS/Introspect_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Introspect_macOS.h
+//  Introspect macOS
+//
+//  Created by Lois Di Qual on 2/11/20.
+//  Copyright © 2020 Lois Di Qual. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Introspect_macOS.
+FOUNDATION_EXPORT double Introspect_macOSVersionNumber;
+
+//! Project version string for Introspect_macOS.
+FOUNDATION_EXPORT const unsigned char Introspect_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Introspect_macOS/PublicHeader.h>
+
+

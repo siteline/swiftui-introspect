3846ed81c8d2e242fc77a28cefe5c808e8481b3f
Marco Quinten <splittydev@gmail.com>
2021-03-16 16:51:11 +0100

    Improve GitHub CI runtime (#82)
    * Build fast on GitHub
    * Skip build for github ci
    * Increase AppKit timeout to 5
    * Add try_scan
    * Disable parallel testing
    * Use multi_scan instead of try_scan
    * Use normal scan for macOS
    * Fix seemingly random test failures on macOS
    * Also add `layoutIfNeeded` to iOS/tvOS tests

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem "cocoapods", "1.10.1"
 gem "fastlane", "~> 2.137"
+
+plugins_path = File.join(File.dirname(__FILE__), 'fastlane', 'Pluginfile')
+eval_gemfile(plugins_path) if File.exist?(plugins_path)
--- IntrospectTests/AppKitTests.swift
+++ IntrospectTests/AppKitTests.swift
@@ -7,7 +7,7 @@ import SwiftUI
 @available(macOS 10.15.0, *)
 enum TestUtils {
     enum Constants {
-        static let timeout: TimeInterval = 2
+        static let timeout: TimeInterval = 5
     }
     
     static func present<ViewType: View>(view: ViewType) {
@@ -20,6 +20,7 @@ enum TestUtils {
         window.setFrameAutosaveName("Main Window")
         window.contentView = NSHostingView(rootView: view)
         window.makeKeyAndOrderFront(nil)
+        window.layoutIfNeeded()
     }
 }
 

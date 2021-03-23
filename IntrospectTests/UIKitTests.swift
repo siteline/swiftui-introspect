d1f39604c5f80af9e4cc2ce1f4341a44040e8cae
JannThomas <jannschafranek@icloud.com>
2021-03-23 18:28:56 +0100

    Fix Tests on iPad

--- IntrospectTests/UIKitTests.swift
+++ IntrospectTests/UIKitTests.swift
@@ -45,6 +45,7 @@ private struct NavigationTestView: View {
                 self.spy()
             }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -56,9 +57,10 @@ private struct ViewControllerTestView: View {
             VStack {
                 EmptyView()
             }
-            .introspectViewController { viewController in
-                self.spy()
-            }
+        }
+        .introspectViewController { viewController in
+            print("oil:", viewController)
+            self.spy()
         }
     }
 }

6d3d130351cc603fd2c9242dd5a75db075d8e587
David Roman <2538074+davdroman@users.noreply.github.com>
2023-02-11 20:42:14 +0000

    wip
    wip

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "Introspect",
     platforms: [
-        .macOS(.v10_15),
         .iOS(.v13),
+        .macOS(.v10_15),
         .tvOS(.v13),
     ],
     products: [

d2339a4a53c5bb657014507131168b4a702e3e78
David Roman <2538074+davdroman@users.noreply.github.com>
2023-03-10 01:00:13 +0000

    wip

--- Examples/Showcase/Showcase/App.swift
+++ Examples/Showcase/Showcase/App.swift
@@ -3,8 +3,6 @@ import SwiftUIIntrospection
 
 @main
 struct App: SwiftUI.App {
-
-
     var body: some Scene {
         WindowGroup {
             Something()
@@ -18,21 +16,27 @@ struct Something: View {
 
     var body: some View {
 //        NavigationView {
-            VStack {
-                Picker("Color", selection: $color, content: {
-                    Text("Red").tag(Color.red)
-                    Text("Green").tag(Color.green)
-                    Text("Blue").tag(Color.blue)
-                })
-                TextField("dq", text: $text)
-                    .introspectTextField { textField in
-                        textField.backgroundColor = UIColor(color)
-                    }
-                TextField("dq", text: $text)
-                    .introspectTextField { textField in
-                        textField.backgroundColor = UIColor(color)
-                    }
+//            List {
+        HStack {
+            Picker("Color", selection: $color) {
+                Text("Red").tag(Color.red)
+                Text("Green").tag(Color.green)
+                Text("Blue").tag(Color.blue)
             }
+            Spacer()
+            TextField("dq", text: $text)
+                .background(Color.green)
+//                .introspectTextField { textField in
+//                    textField.backgroundColor = UIColor(color)
+//                }
+                .fixedSize()
+        }
+                TextField("dq", text: $text)
+//                    .background(Color.green)
+                    .introspectTextField { textField in
+                        textField.backgroundColor = UIColor(color)
+                    }
+//            }
 //        }
     }
 }

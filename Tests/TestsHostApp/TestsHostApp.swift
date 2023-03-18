fc7551d76554826465738015689c3abff2f12435
David Roman <2538074+davdroman@users.noreply.github.com>
2023-03-18 18:13:43 +0000

    wip

--- Tests/TestsHostApp/TestsHostApp.swift
+++ Tests/TestsHostApp/TestsHostApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+#if os(iOS) || os(tvOS)
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -7,16 +8,33 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UIHostingController(rootView: {
-            VStack(spacing: 20) {
-                Text("Host App for Tests").bold()
-                Text("This is just an app target to run tests against, needed for iOS 13 compatibility.")
-                Text("If iOS 13 support is dropped in the future, this target can and should be removed and tests should be ran using SPM instead.")
-            }
-            .multilineTextAlignment(.center)
-            .padding()
-        }())
+        window?.rootViewController = UIHostingController(rootView: AppView())
         window?.makeKeyAndVisible()
         return true
+    }
+}
+#elseif os(macOS)
+@main
+struct App: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
+#endif
+
+struct AppView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Host App for Tests").bold()
+            Text("This is just an app target to run tests against, needed for iOS 13 compatibility.")
+            Text("If iOS 13 support is dropped in the future, this target can and should be removed and tests should be ran using SPM instead.")
+        }
+        .multilineTextAlignment(.center)
+        .padding()
+        #if os(macOS)
+        .fixedSize()
+        #endif
     }
 }

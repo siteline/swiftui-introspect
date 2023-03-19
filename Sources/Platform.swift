5d34321eaec5ee3ae3694dc06da30d30b8365685
David Roman <2538074+davdroman@users.noreply.github.com>
2023-03-19 13:39:36 +0000

    refactor platform version as struct

--- Sources/Platform.swift
+++ Sources/Platform.swift
@@ -21,116 +21,151 @@ public protocol PlatformVersion {
     var isCurrent: Bool { get }
 }
 
-public enum iOSVersion: PlatformVersion {
-    case v13, v14, v15, v16
+public struct iOSVersion: PlatformVersion {
+    public let isCurrent: Bool
 
-    public var isCurrent: Bool {
-        switch self {
-        case .v13:
-            if #available(iOS 14, *) {
-                return false
-            }
-            if #available(iOS 13, *) {
-                return true
-            }
-        case .v14:
-            if #available(iOS 15, *) {
-                return false
-            }
-            if #available(iOS 14, *) {
-                return true
-            }
-        case .v15:
-            if #available(iOS 16, *) {
-                return false
-            }
-            if #available(iOS 15, *) {
-                return true
-            }
-        case .v16:
-            if #available(iOS 17, *) {
-                return false
-            }
-            if #available(iOS 16, *) {
-                return true
-            }
+    public init(isCurrent: () -> Bool) {
+        self.isCurrent = isCurrent()
+    }
+}
+
+extension iOSVersion {
+    public static let v13 = iOSVersion {
+        if #available(iOS 14, *) {
+            return false
+        }
+        if #available(iOS 13, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v14 = iOSVersion {
+        if #available(iOS 15, *) {
+            return false
+        }
+        if #available(iOS 14, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v15 = iOSVersion {
+        if #available(iOS 16, *) {
+            return false
+        }
+        if #available(iOS 15, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v16 = iOSVersion {
+        if #available(iOS 17, *) {
+            return false
+        }
+        if #available(iOS 16, *) {
+            return true
         }
         return false
     }
 }
 
+public struct tvOSVersion: PlatformVersion {
+    public let isCurrent: Bool
 
-public enum tvOSVersion: PlatformVersion {
-    case v13, v14, v15, v16
+    public init(isCurrent: () -> Bool) {
+        self.isCurrent = isCurrent()
+    }
+}
 
-    public var isCurrent: Bool {
-        switch self {
-        case .v13:
-            if #available(tvOS 14, *) {
-                return false
-            }
-            if #available(tvOS 13, *) {
-                return true
-            }
-        case .v14:
-            if #available(tvOS 15, *) {
-                return false
-            }
-            if #available(tvOS 14, *) {
-                return true
-            }
-        case .v15:
-            if #available(tvOS 16, *) {
-                return false
-            }
-            if #available(tvOS 15, *) {
-                return true
-            }
-        case .v16:
-            if #available(tvOS 17, *) {
-                return false
-            }
-            if #available(tvOS 16, *) {
-                return true
-            }
+extension tvOSVersion {
+    public static let v13 = tvOSVersion {
+        if #available(tvOS 14, *) {
+            return false
+        }
+        if #available(tvOS 13, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v14 = tvOSVersion {
+        if #available(tvOS 15, *) {
+            return false
+        }
+        if #available(tvOS 14, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v15 = tvOSVersion {
+        if #available(tvOS 16, *) {
+            return false
+        }
+        if #available(tvOS 15, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v16 = tvOSVersion {
+        if #available(tvOS 17, *) {
+            return false
+        }
+        if #available(tvOS 16, *) {
+            return true
         }
         return false
     }
 }
 
-public enum macOSVersion: PlatformVersion {
-    case v10_15, v11, v12, v13
+public struct macOSVersion: PlatformVersion {
+    public let isCurrent: Bool
 
-    public var isCurrent: Bool {
-        switch self {
-        case .v10_15:
-            if #available(macOS 11, *) {
-                return false
-            }
-            if #available(macOS 10.15, *) {
-                return true
-            }
-        case .v11:
-            if #available(macOS 12, *) {
-                return false
-            }
-            if #available(macOS 11, *) {
-                return true
-            }
-        case .v12:
-            if #available(macOS 13, *) {
-                return false
-            }
-            if #available(macOS 12, *) {
-                return true
-            }
-        case .v13:
-            if #available(macOS 14, *) {
-                return false
-            }
-            if #available(macOS 13, *) {
-                return true
-            }
+    public init(isCurrent: () -> Bool) {
+        self.isCurrent = isCurrent()
+    }
+}
+
+extension macOSVersion {
+    public static let v10_15 = macOSVersion {
+        if #available(macOS 11, *) {
+            return false
+        }
+        if #available(macOS 10.15, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v11 = macOSVersion {
+        if #available(macOS 12, *) {
+            return false
+        }
+        if #available(macOS 11, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v12 = macOSVersion {
+        if #available(macOS 13, *) {
+            return false
+        }
+        if #available(macOS 12, *) {
+            return true
+        }
+        return false
+    }
+
+    public static let v13 = macOSVersion {
+        if #available(macOS 14, *) {
+            return false
+        }
+        if #available(macOS 13, *) {
+            return true
         }
         return false
     }

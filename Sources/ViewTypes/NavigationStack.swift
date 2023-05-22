2f2ed8f375fd33f5059ec565d1739f0ffa186840
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-22 19:36:48 +0100

    wip

--- Sources/ViewTypes/NavigationStack.swift
+++ Sources/ViewTypes/NavigationStack.swift
@@ -1,43 +1,27 @@
-//import SwiftUI
-//
-//// MARK: SwiftUI.NavigationStack
-//
-//@available(iOS, introduced: 16)
-//@available(tvOS, introduced: 16)
-//@available(macOS, introduced: 13)
-//public struct NavigationStackType: ViewType {}
-//
-//@available(iOS, introduced: 16)
-//@available(tvOS, introduced: 16)
-//@available(macOS, introduced: 13)
-//extension StaticMember where Base == NavigationStackType {
-//    public static var navigationStack: Self { .init(base: .init()) }
-//}
-//
-//// MARK: SwiftUI.List - iOS
-//
-//#if canImport(UIKit)
-//import UIKit
-//
-////@available(iOS, introduced: 16)
-//@available(iOS 16, *)
-//extension PlatformVersionDescriptor where Version == iOSVersion, SwiftUIView == NavigationStackType, PlatformView == UINavigationController {
-//    public static let v16 = Self(for: .v16) { customize in
-//        UIKitIntrospectionViewController(
-//            selector: { introspectionViewController in
-//
-//                // Search in ancestors
-//                if let navigationController = introspectionViewController.navigationController {
-//                    return navigationController
-//                }
-//
-//                // Search in siblings
-//                return Introspect.previousSibling(containing: UINavigationController.self, from: introspectionViewController)
-//            },
-//            customize: customize
-//        )
-//    }
-//}
-//#endif
-//
-//// TODO: tvOS and macOS
+import SwiftUI
+
+// MARK: SwiftUI.NavigationStack
+
+public struct NavigationStackType: ViewType {
+    public static let scope: IntrospectionScope = .receiverOrAncestor
+}
+
+extension ViewType where Self == NavigationStackType {
+    public static var navigationStack: Self { .init() }
+}
+
+// MARK: SwiftUI.NavigationStack - iOS
+
+#if canImport(UIKit)
+extension PlatformVersionDescriptor where Version == iOSVersion, SwiftUIView == NavigationStackType, PlatformView == UINavigationController {
+    public static let v16 = Self(for: .v16)
+}
+#endif
+
+// MARK: SwiftUI.NavigationStack - tvOS
+
+#if canImport(UIKit)
+extension PlatformVersionDescriptor where Version == tvOSVersion, SwiftUIView == NavigationStackType, PlatformView == UINavigationController {
+    public static let v16 = Self(for: .v16)
+}
+#endif

8118f565dbba79751b4e063bbd23e52656e9b03b
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-23 23:15:02 +0100

    refactor

--- Tests/Tests/ViewTypes/ScrollViewTests.swift
+++ Tests/Tests/ViewTypes/ScrollViewTests.swift
@@ -9,237 +9,122 @@ final class ScrollViewTests: XCTestCase {
     typealias PlatformScrollView = NSScrollView
     #endif
 
-    func testScrollView() throws {
-        struct ScrollTestView: View {
-            let spy1: (PlatformScrollView) -> Void
-            let spy2: (PlatformScrollView) -> Void
+    func testScrollView() {
+        XCTAssertViewIntrospection(of: PlatformScrollView.self) { spies in
+            let spy0 = spies[0]
+            let spy1 = spies[1]
 
-            var body: some View {
-                HStack {
-                    ScrollView(showsIndicators: false) {
-                        Text("Item 1")
-                    }
-                    #if os(iOS) || os(tvOS)
-                    .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                        self.spy1(scrollView)
-                    }
-                    #elseif os(macOS)
-                    .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                        self.spy1(scrollView)
-                    }
-                    #endif
-                    ScrollView(showsIndicators: true) {
-                        Text("Item 1")
-                        #if os(iOS) || os(tvOS)
-                        .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                            self.spy2(scrollView)
-                        }
-                        #elseif os(macOS)
-                        .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                            self.spy2(scrollView)
-                        }
-                        #endif
-                    }
-                }
-            }
-        }
-
-        let expectation1 = XCTestExpectation()
-        let expectation2 = XCTestExpectation()
-
-        var scrollView1: PlatformScrollView?
-        var scrollView2: PlatformScrollView?
-
-        let view = ScrollTestView(
-            spy1: {
-                if let scrollView1 = scrollView1 {
-                    XCTAssert(scrollView1 === $0)
-                }
-                scrollView1 = $0
-                expectation1.fulfill()
-            },
-            spy2: {
-                if let scrollView2 = scrollView2 {
-                    XCTAssert(scrollView2 === $0)
-                }
-                scrollView2 = $0
-                expectation2.fulfill()
-            }
-        )
-        TestUtils.present(view: view)
-        wait(for: [expectation1, expectation2], timeout: TestUtils.Constants.timeout)
-
-        let unwrappedScrollView1 = try XCTUnwrap(scrollView1)
-        let unwrappedScrollView2 = try XCTUnwrap(scrollView2)
-
-        #if canImport(UIKit)
-        XCTAssertEqual(unwrappedScrollView1.showsVerticalScrollIndicator, false)
-        XCTAssertEqual(unwrappedScrollView2.showsVerticalScrollIndicator, true)
-        #elseif canImport(AppKit)
-        // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
-        if #available(macOS 13, *) {
-            XCTAssert(unwrappedScrollView1.verticalScroller == nil)
-            XCTAssert(unwrappedScrollView2.verticalScroller != nil)
-        }
-        #endif
-
-        XCTAssert(unwrappedScrollView1 !== unwrappedScrollView2)
-    }
-
-    func testNestedScrollView() throws {
-        struct NestedScrollTestView: View {
-            let spy1: (PlatformScrollView) -> Void
-            let spy2: (PlatformScrollView) -> Void
-
-            var body: some View {
-                ScrollView(showsIndicators: true) {
+            HStack {
+                ScrollView(showsIndicators: false) {
                     Text("Item 1")
-
-                    ScrollView(showsIndicators: false) {
-                        Text("Item 1")
-                    }
-                    #if os(iOS) || os(tvOS)
-                    .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                        self.spy2(scrollView)
-                    }
-                    #elseif os(macOS)
-                    .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                        self.spy2(scrollView)
-                    }
-                    #endif
                 }
                 #if os(iOS) || os(tvOS)
-                .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                    self.spy1(scrollView)
-                }
+                .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy0)
                 #elseif os(macOS)
-                .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                    self.spy1(scrollView)
-                }
+                .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy0)
                 #endif
-            }
-        }
-
-        let expectation1 = XCTestExpectation()
-        let expectation2 = XCTestExpectation()
-
-        var scrollView1: PlatformScrollView?
-        var scrollView2: PlatformScrollView?
-
-        let view = NestedScrollTestView(
-            spy1: {
-                if let scrollView1 = scrollView1 {
-                    XCTAssert(scrollView1 === $0)
+                ScrollView(showsIndicators: true) {
+                    Text("Item 1")
+                    #if os(iOS) || os(tvOS)
+                    .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy1)
+                    #elseif os(macOS)
+                    .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy1)
+                    #endif
                 }
-                scrollView1 = $0
-                expectation1.fulfill()
-            },
-            spy2: {
-                if let scrollView2 = scrollView2 {
-                    XCTAssert(scrollView2 === $0)
-                }
-                scrollView2 = $0
-                expectation2.fulfill()
             }
-        )
-        TestUtils.present(view: view)
-        wait(for: [expectation1, expectation2], timeout: TestUtils.Constants.timeout)
+        } extraAssertions: {
+            #if canImport(UIKit)
+            XCTAssertEqual($0[safe: 0]?.showsVerticalScrollIndicator, false)
+            XCTAssertEqual($0[safe: 1]?.showsVerticalScrollIndicator, true)
+            #elseif canImport(AppKit)
+            // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
+            if #available(macOS 13, *) {
+                XCTAssert($0[safe: 0]?.verticalScroller == nil)
+                XCTAssert($0[safe: 1]?.verticalScroller != nil)
+            }
+            #endif
 
-        let unwrappedScrollView1 = try XCTUnwrap(scrollView1)
-        let unwrappedScrollView2 = try XCTUnwrap(scrollView2)
-
-        #if canImport(UIKit)
-        XCTAssertEqual(unwrappedScrollView1.showsVerticalScrollIndicator, true)
-        XCTAssertEqual(unwrappedScrollView2.showsVerticalScrollIndicator, false)
-        #elseif canImport(AppKit)
-        // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
-        if #available(macOS 13, *) {
-            XCTAssert(unwrappedScrollView1.verticalScroller != nil)
-            XCTAssert(unwrappedScrollView2.verticalScroller == nil)
+            XCTAssert($0[safe: 0] !== $0[safe: 1])
         }
-        #endif
-
-        XCTAssert(unwrappedScrollView1 !== unwrappedScrollView2)
     }
 
-    func testMaskedScrollView() throws {
-        struct MaskedScrollTestView: View {
-            let spy1: (PlatformScrollView) -> Void
-            let spy2: (PlatformScrollView) -> Void
+    func testNestedScrollView() {
+        XCTAssertViewIntrospection(of: PlatformScrollView.self) { spies in
+            let spy0 = spies[0]
+            let spy1 = spies[1]
 
-            var body: some View {
-                HStack {
-                    ScrollView(showsIndicators: false) {
-                        Text("Item 1")
-                    }
-                    #if os(iOS) || os(tvOS)
-                    .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                        self.spy1(scrollView)
-                    }
-                    #elseif os(macOS)
-                    .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                        self.spy1(scrollView)
-                    }
-                    #endif
-                    .clipped()
-                    .clipShape(RoundedRectangle(cornerRadius: 20.0))
-                    .cornerRadius(2.0)
-                    ScrollView(showsIndicators: true) {
-                        Text("Item 1")
-                            #if os(iOS) || os(tvOS)
-                            .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16)) { scrollView in
-                                self.spy2(scrollView)
-                            }
-                            #elseif os(macOS)
-                            .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13)) { scrollView in
-                                self.spy2(scrollView)
-                            }
-                            #endif
-                    }
+            ScrollView(showsIndicators: true) {
+                Text("Item 1")
+
+                ScrollView(showsIndicators: false) {
+                    Text("Item 1")
+                }
+                #if os(iOS) || os(tvOS)
+                .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy1)
+                #elseif os(macOS)
+                .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy1)
+                #endif
+            }
+            #if os(iOS) || os(tvOS)
+            .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy0)
+            #elseif os(macOS)
+            .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy0)
+            #endif
+        } extraAssertions: {
+            #if canImport(UIKit)
+            XCTAssertEqual($0[safe: 0]?.showsVerticalScrollIndicator, true)
+            XCTAssertEqual($0[safe: 1]?.showsVerticalScrollIndicator, false)
+            #elseif canImport(AppKit)
+            // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
+            if #available(macOS 13, *) {
+                XCTAssert($0[safe: 0]?.verticalScroller != nil)
+                XCTAssert($0[safe: 1]?.verticalScroller == nil)
+            }
+            #endif
+
+            XCTAssert($0[safe: 0] !== $0[safe: 1])
+        }
+    }
+
+    func testMaskedScrollView() {
+        XCTAssertViewIntrospection(of: PlatformScrollView.self) { spies in
+            let spy0 = spies[0]
+            let spy1 = spies[1]
+
+            HStack {
+                ScrollView(showsIndicators: false) {
+                    Text("Item 1")
+                }
+                #if os(iOS) || os(tvOS)
+                .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy0)
+                #elseif os(macOS)
+                .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy0)
+                #endif
+                .clipped()
+                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                .cornerRadius(2.0)
+                ScrollView(showsIndicators: true) {
+                    Text("Item 1")
+                        #if os(iOS) || os(tvOS)
+                        .introspect(.scrollView, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy1)
+                        #elseif os(macOS)
+                        .introspect(.scrollView, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy1)
+                        #endif
                 }
             }
-        }
-
-        let expectation1 = XCTestExpectation()
-        let expectation2 = XCTestExpectation()
-
-        var scrollView1: PlatformScrollView?
-        var scrollView2: PlatformScrollView?
-
-        let view = MaskedScrollTestView(
-            spy1: {
-                if let scrollView1 = scrollView1 {
-                    XCTAssert(scrollView1 === $0)
-                }
-                scrollView1 = $0
-                expectation1.fulfill()
-            },
-            spy2: {
-                if let scrollView2 = scrollView2 {
-                    XCTAssert(scrollView2 === $0)
-                }
-                scrollView2 = $0
-                expectation2.fulfill()
+        } extraAssertions: {
+            #if canImport(UIKit)
+            XCTAssertEqual($0[safe: 0]?.showsVerticalScrollIndicator, false)
+            XCTAssertEqual($0[safe: 1]?.showsVerticalScrollIndicator, true)
+            #elseif canImport(AppKit)
+            // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
+            if #available(macOS 13, *) {
+                XCTAssert($0[safe: 0]?.verticalScroller == nil)
+                XCTAssert($0[safe: 1]?.verticalScroller != nil)
             }
-        )
+            #endif
 
-        TestUtils.present(view: view)
-        wait(for: [expectation1, expectation2], timeout: TestUtils.Constants.timeout)
-
-        let unwrappedScrollView1 = try XCTUnwrap(scrollView1)
-        let unwrappedScrollView2 = try XCTUnwrap(scrollView2)
-
-        #if canImport(UIKit)
-        XCTAssertEqual(unwrappedScrollView1.showsVerticalScrollIndicator, false)
-        XCTAssertEqual(unwrappedScrollView2.showsVerticalScrollIndicator, true)
-        #elseif canImport(AppKit)
-        // FIXME: these assertions don't pass on macOS 12, not sure why... maybe callback is too premature in relation to view lifecycle?
-        if #available(macOS 13, *) {
-            XCTAssert(unwrappedScrollView1.verticalScroller == nil)
-            XCTAssert(unwrappedScrollView2.verticalScroller != nil)
+            XCTAssert($0[safe: 0] !== $0[safe: 1])
         }
-        #endif
-
-        XCTAssert(unwrappedScrollView1 !== unwrappedScrollView2)
     }
 }

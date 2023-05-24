af115d70a14e84816e9adf19e2d710f2e32db3f1
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-24 18:10:43 +0100

    wip

--- Tests/Tests/TestUtils.swift
+++ Tests/Tests/TestUtils.swift
@@ -50,72 +50,70 @@ enum TestUtils {
 }
 #endif
 
-extension XCTestCase {
-    func XCTAssertViewIntrospection<V: View, PV: AnyObject>(
-        of type: PV.Type,
-        @ViewBuilder view: (Spies<PV>) -> V,
-        extraAssertions: ([PV]) -> Void = { _ in },
+func XCTAssertViewIntrospection<V: View, PV: AnyObject>(
+    of type: PV.Type,
+    @ViewBuilder view: (Spies<PV>) -> V,
+    extraAssertions: ([PV]) -> Void = { _ in },
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    let spies = Spies<PV>()
+    let view = view(spies)
+    TestUtils.present(view: view)
+    XCTWaiter(delegate: spies).wait(for: spies.expectations.values.map(\.0), timeout: TestUtils.Constants.timeout)
+    extraAssertions(spies.objects.sorted(by: { $0.key < $1.key }).map(\.value))
+}
+
+final class Spies<PV: AnyObject>: NSObject, XCTWaiterDelegate {
+    private(set) var objects: [Int: PV] = [:]
+    private(set) var expectations: [ObjectIdentifier: (XCTestExpectation, StaticString, UInt)] = [:]
+
+    subscript(
+        number: Int,
         file: StaticString = #file,
         line: UInt = #line
-    ) {
-        let spies = Spies<PV>()
-        let view = view(spies)
-        TestUtils.present(view: view)
-        XCTWaiter(delegate: spies).wait(for: spies.expectations.values.map(\.0), timeout: TestUtils.Constants.timeout)
-        extraAssertions(spies.objects.sorted(by: { $0.key < $1.key }).map(\.value))
+    ) -> (PV) -> Void {
+        let expectation = XCTestExpectation()
+        expectations[ObjectIdentifier(expectation)] = (expectation, file, line)
+        return { [self] in
+            if let object = objects[number] {
+                XCTAssert(object === $0, "Found view was overriden by another view", file: file, line: line)
+            }
+            objects[number] = $0
+            expectation.fulfill()
+        }
     }
 
-    final class Spies<PV: AnyObject>: NSObject, XCTWaiterDelegate {
-        private(set) var objects: [Int: PV] = [:]
-        private(set) var expectations: [ObjectIdentifier: (XCTestExpectation, StaticString, UInt)] = [:]
-
-        subscript(
-            number: Int,
-            file: StaticString = #file,
-            line: UInt = #line
-        ) -> (PV) -> Void {
-            let expectation = XCTestExpectation()
-            expectations[ObjectIdentifier(expectation)] = (expectation, file, line)
-            return { [self] in
-                if let object = objects[number] {
-                    XCTAssert(object === $0, "Found view was overriden by another view", file: file, line: line)
-                }
-                objects[number] = $0
-                expectation.fulfill()
-            }
+    func waiter(
+        _ waiter: XCTWaiter,
+        didTimeoutWithUnfulfilledExpectations unfulfilledExpectations: [XCTestExpectation]
+    ) {
+        for expectation in unfulfilledExpectations {
+            let (_, file, line) = expectations[ObjectIdentifier(expectation)]!
+            XCTFail("Spy not called", file: file, line: line)
         }
+    }
 
-        func waiter(
-            _ waiter: XCTWaiter,
-            didTimeoutWithUnfulfilledExpectations unfulfilledExpectations: [XCTestExpectation]
-        ) {
-            for expectation in unfulfilledExpectations {
-                let (_, file, line) = expectations[ObjectIdentifier(expectation)]!
-                XCTFail("Spy not called", file: file, line: line)
-            }
-        }
+    func nestedWaiter(
+        _ waiter: XCTWaiter,
+        wasInterruptedByTimedOutWaiter outerWaiter: XCTWaiter
+    ) {
+        XCTFail("wasInterruptedByTimedOutWaiter")
+    }
 
-        func nestedWaiter(
-            _ waiter: XCTWaiter,
-            wasInterruptedByTimedOutWaiter outerWaiter: XCTWaiter
-        ) {
-            XCTFail("wasInterruptedByTimedOutWaiter")
-        }
+    func waiter(
+        _ waiter: XCTWaiter,
+        fulfillmentDidViolateOrderingConstraintsFor expectation: XCTestExpectation,
+        requiredExpectation: XCTestExpectation
+    ) {
+        XCTFail("fulfillmentDidViolateOrderingConstraintsFor")
+    }
 
-        func waiter(
-            _ waiter: XCTWaiter,
-            fulfillmentDidViolateOrderingConstraintsFor expectation: XCTestExpectation,
-            requiredExpectation: XCTestExpectation
-        ) {
-            XCTFail("fulfillmentDidViolateOrderingConstraintsFor")
-        }
-
-        func waiter(
-            _ waiter: XCTWaiter,
-            didFulfillInvertedExpectation expectation: XCTestExpectation
-        ) {
-            XCTFail("didFulfillInvertedExpectation")
-        }
+    func waiter(
+        _ waiter: XCTWaiter,
+        didFulfillInvertedExpectation expectation: XCTestExpectation
+    ) {
+        XCTFail("didFulfillInvertedExpectation")
     }
 }
 

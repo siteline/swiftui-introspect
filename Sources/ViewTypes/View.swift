ae77b2113e113f92de60546c6ab0c8203862e908
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-27 22:28:31 +0100

    wip

--- Sources/ViewTypes/View.swift
+++ Sources/ViewTypes/View.swift
@@ -43,11 +43,4 @@ extension macOSViewVersion<ViewType, NSView> {
     public static let v12 = Self(for: .v12)
     public static let v13 = Self(for: .v13)
 }
-
-extension macOSViewVersion<ViewType, NSViewController> {
-    public static let v10_15 = Self(for: .v10_15)
-    public static let v11 = Self(for: .v11)
-    public static let v12 = Self(for: .v12)
-    public static let v13 = Self(for: .v13)
-}
 #endif
--- Tests/Tests/ViewTypes/ViewTests.swift
+++ Tests/Tests/ViewTypes/ViewTests.swift
@@ -28,6 +28,7 @@ final class ViewTests: XCTestCase {
         }
     }
 
+    #if !os(macOS)
     func testViewAsViewController() {
         XCTAssertViewIntrospection(of: PlatformViewController.self) { spies in
             let spy0 = spies[0]
@@ -38,24 +39,19 @@ final class ViewTests: XCTestCase {
                     Text("Item 0")
                         #if os(iOS) || os(tvOS)
                         .introspect(.view, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy0)
-                        #elseif os(macOS)
-                        .introspect(.view, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy0)
                         #endif
                 }
-                .navigationViewStyle(.stack)
 
                 NavigationView {
                     Text("Item 1")
                         #if os(iOS) || os(tvOS)
                         .introspect(.view, on: .iOS(.v13, .v14, .v15, .v16), .tvOS(.v13, .v14, .v15, .v16), customize: spy1)
-                        #elseif os(macOS)
-                        .introspect(.view, on: .macOS(.v10_15, .v11, .v12, .v13), customize: spy1)
                         #endif
                 }
-                .navigationViewStyle(.stack)
             }
         } extraAssertions: {
             XCTAssert($0[safe: 0] !== $0[safe: 1])
         }
     }
+    #endif
 }

6b90f992963d5caffcc1b5e3067be6321243615d
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-28 17:35:45 +0100

    wip

--- Sources/ViewTypes/ColorPicker.swift
+++ Sources/ViewTypes/ColorPicker.swift
@@ -13,7 +13,7 @@ extension IntrospectableViewType where Self == ColorPickerType {
 @available(iOS 14, *)
 extension iOSViewVersion<ColorPickerType, UIColorWell> {
     @available(*, unavailable, message: "ColorPicker isn't available on iOS 13")
-    public static let v13 = Self(for: .v13)
+    public static let v13 = Self.unavailable()
     public static let v14 = Self(for: .v14)
     public static let v15 = Self(for: .v15)
     public static let v16 = Self(for: .v16)
@@ -22,7 +22,7 @@ extension iOSViewVersion<ColorPickerType, UIColorWell> {
 @available(macOS 11, *)
 extension macOSViewVersion<ColorPickerType, NSColorWell> {
     @available(*, unavailable, message: "ColorPicker isn't available on macOS 10.15")
-    public static let v10_15 = Self(for: .v10_15)
+    public static let v10_15 = Self.unavailable()
     public static let v11 = Self(for: .v11)
     public static let v12 = Self(for: .v12)
     public static let v13 = Self(for: .v13)
--- Sources/ViewTypes/TabViewWithPageStyle.swift
+++ Sources/ViewTypes/TabViewWithPageStyle.swift
@@ -11,12 +11,16 @@ extension IntrospectableViewType where Self == TabViewWithPageStyleType {
 
 #if canImport(UIKit)
 extension iOSViewVersion<TabViewWithPageStyleType, UICollectionView> {
+    @available(*, unavailable, message: "TabView {}.tabViewStyle(.page) isn't available on iOS 13")
+    public static let v13 = Self.unavailable()
     public static let v14 = Self(for: .v14)
     public static let v15 = Self(for: .v15)
     public static let v16 = Self(for: .v16)
 }
 
 extension tvOSViewVersion<TabViewWithPageStyleType, UICollectionView> {
+    @available(*, unavailable, message: "TabView {}.tabViewStyle(.page) isn't available on tvOS 13")
+    public static let v13 = Self.unavailable()
     public static let v14 = Self(for: .v14)
     public static let v15 = Self(for: .v15)
     public static let v16 = Self(for: .v16)

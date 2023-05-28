5beb6e306e2369d60df6de0c877f8b97e561a7f7
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-28 16:39:22 +0100

    wip

---
+++ Sources/ViewTypes/DatePicker.swift
@@ -0,0 +1,27 @@
+#if !os(tvOS)
+import SwiftUI
+
+// MARK: SwiftUI.DatePicker
+
+public struct DatePickerType: IntrospectableViewType {}
+
+extension IntrospectableViewType where Self == DatePickerType {
+    public static var datePicker: Self { .init() }
+}
+
+#if canImport(UIKit)
+extension iOSViewVersion<DatePickerType, UIDatePicker> {
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+}
+#elseif canImport(AppKit)
+extension macOSViewVersion<DatePickerType, NSDatePicker> {
+    public static let v10_15 = Self(for: .v10_15)
+    public static let v11 = Self(for: .v11)
+    public static let v12 = Self(for: .v12)
+    public static let v13 = Self(for: .v13)
+}
+#endif
+#endif

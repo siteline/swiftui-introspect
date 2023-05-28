0b3db37d51ab886c906908c285f7675763d8d860
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-28 15:27:38 +0100

    wip

---
+++ Sources/ViewTypes/TextEditor.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+// MARK: SwiftUI.TextEditor
+
+public struct TextEditorType: IntrospectableViewType {}
+
+extension IntrospectableViewType where Self == TextEditorType {
+    public static var textEditor: Self { .init() }
+}
+
+#if canImport(UIKit)
+extension iOSViewVersion<TextEditorType, UITextView> {
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+}
+
+extension tvOSViewVersion<TextEditorType, UITextView> {
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+}
+#elseif canImport(AppKit)
+extension macOSViewVersion<TextEditorType, NSTextView> {
+    public static let v11 = Self(for: .v11)
+    public static let v12 = Self(for: .v12)
+    public static let v13 = Self(for: .v13)
+}
+#endif

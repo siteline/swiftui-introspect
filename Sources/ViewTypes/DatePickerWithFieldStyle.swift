bc6d83d716c36a79e70e7b954c347c4e306ef1c9
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-31 22:33:42 +0100

    wip

---
+++ Sources/ViewTypes/DatePickerWithFieldStyle.swift
@@ -0,0 +1,24 @@
+#if os(macOS)
+import SwiftUI
+
+// MARK: SwiftUI.DatePicker { ... }.datePickerStyle(.field)
+
+public struct DatePickerWithFieldStyleType: IntrospectableViewType {
+    public enum Style {
+        case field
+    }
+}
+
+extension IntrospectableViewType where Self == DatePickerWithFieldStyleType {
+    public static func datePicker(style: Self.Style) -> Self { .init() }
+}
+
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
+extension macOSViewVersion<DatePickerWithFieldStyleType, NSDatePicker> {
+    public static let v10_15 = Self(for: .v10_15)
+    public static let v11 = Self(for: .v11)
+    public static let v12 = Self(for: .v12)
+    public static let v13 = Self(for: .v13)
+}
+#endif
+#endif

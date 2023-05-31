b14e00ea0dc014ab4653817f7fc8d6efe211c998
David Roman <2538074+davdroman@users.noreply.github.com>
2023-05-31 20:27:08 +0100

    wip

--- Sources/ViewTypes/ToggleWithButtonStyle.swift
+++ Sources/ViewTypes/ToggleWithButtonStyle.swift
@@ -16,9 +16,9 @@ extension IntrospectableViewType where Self == ToggleWithButtonStyleType {
 #if canImport(AppKit) && !targetEnvironment(macCatalyst)
 extension macOSViewVersion<ToggleWithButtonStyleType, NSButton> {
     @available(*, unavailable, message: ".toggleStyle(.button) isn't available on macOS 10.15")
-    public static let v10_15 = Self(for: .v10_15)
+    public static let v10_15 = Self.unavailable()
     @available(*, unavailable, message: ".toggleStyle(.button) isn't available on macOS 11")
-    public static let v11 = Self(for: .v11)
+    public static let v11 = Self.unavailable()
     public static let v12 = Self(for: .v12)
     public static let v13 = Self(for: .v13)
 }

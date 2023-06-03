0e0dc8e4860406ed55d7e25ad7409758cd776260
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-03 13:48:25 +0100

    wip

--- Sources/IntrospectionSelector.swift
+++ Sources/IntrospectionSelector.swift
@@ -1,12 +1,30 @@
 @_spi(Internals)
 public struct IntrospectionSelector<Target: PlatformEntity> {
-    private let selector: (any PlatformEntity, IntrospectionScope, IntrospectionAnchorID) -> Target?
+    private let selector: (IntrospectionPlatformViewController, IntrospectionScope, IntrospectionAnchorID) -> Target?
 
     static var `default`: Self { .from(Target.self, selector: { $0 }) }
 
     @_spi(Internals)
     public static func from<Entry: PlatformEntity>(_ entryType: Entry.Type, selector: @escaping (Entry) -> Target?) -> Self {
-        .init { entity, scope, anchorID in
+        .init { controller, scope, anchorID in
+            guard let entity = { () -> (any PlatformEntity)? in
+                if Entry.Base.self == PlatformView.self {
+                    #if canImport(UIKit)
+                    if let introspectionView = controller.viewIfLoaded {
+                        return introspectionView
+                    }
+                    #elseif canImport(AppKit)
+                    if controller.isViewLoaded {
+                        return controller.view
+                    }
+                    #endif
+                } else if Entry.Base.self == PlatformViewController.self {
+                    return controller
+                }
+                return nil
+            }() else {
+                return nil
+            }
             if
                 scope.contains(.receiver),
                 let entry = entity.receiver(ofType: Entry.self, anchorID: anchorID),
@@ -25,15 +43,15 @@ public struct IntrospectionSelector<Target: PlatformEntity> {
         }
     }
 
-    init(_ selector: @escaping (any PlatformEntity, IntrospectionScope, IntrospectionAnchorID) -> Target?) {
+    init(_ selector: @escaping (IntrospectionPlatformViewController, IntrospectionScope, IntrospectionAnchorID) -> Target?) {
         self.selector = selector
     }
 
     func callAsFunction(
-        _ entity: any PlatformEntity,
+        _ controller: IntrospectionPlatformViewController,
         _ scope: IntrospectionScope,
         _ anchorID: IntrospectionAnchorID
     ) -> Target? {
-        selector(entity, scope, anchorID)
+        selector(controller, scope, anchorID)
     }
 }

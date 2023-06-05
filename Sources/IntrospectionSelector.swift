23b08e51fc5d8ee98beb33a79f8e93e9612e94da
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-05 01:00:14 +0100

    Selector overrides (#239)

--- Sources/IntrospectionSelector.swift
+++ Sources/IntrospectionSelector.swift
@@ -1,50 +1,43 @@
 @_spi(Internals)
 public struct IntrospectionSelector<Target: PlatformEntity> {
-    private let selector: (IntrospectionPlatformViewController, IntrospectionScope, IntrospectionAnchorID) -> Target?
-
-    static var `default`: Self { .from(Target.self, selector: { $0 }) }
+    @_spi(Internals)
+    public static var `default`: Self { .from(Target.self, selector: { $0 }) }
 
     @_spi(Internals)
     public static func from<Entry: PlatformEntity>(_ entryType: Entry.Type, selector: @escaping (Entry) -> Target?) -> Self {
-        .init { controller, scope, anchorID in
-            guard let entity = { () -> (any PlatformEntity)? in
-                if Entry.Base.self == PlatformView.self {
-                    #if canImport(UIKit)
-                    if let introspectionView = controller.viewIfLoaded {
-                        return introspectionView
-                    }
-                    #elseif canImport(AppKit)
-                    if controller.isViewLoaded {
-                        return controller.view
-                    }
-                    #endif
-                } else if Entry.Base.self == PlatformViewController.self {
-                    return controller
-                }
-                return nil
-            }() else {
-                return nil
+        .init(
+            receiverSelector: { controller, anchorID in
+                controller.as(Entry.self)?.receiver(ofType: Entry.self, anchorID: anchorID).flatMap(selector)
+            },
+            ancestorSelector: { controller in
+                controller.as(Entry.self)?.ancestor(ofType: Entry.self).flatMap(selector)
             }
-            if
-                scope.contains(.receiver),
-                let entry = entity.receiver(ofType: Entry.self, anchorID: anchorID),
-                let target = selector(entry)
-            {
-                return target
-            }
-            if
-                scope.contains(.ancestor),
-                let entry = entity.ancestor(ofType: Entry.self),
-                let target = selector(entry)
-            {
-                return target
-            }
-            return nil
-        }
+        )
     }
 
-    init(_ selector: @escaping (IntrospectionPlatformViewController, IntrospectionScope, IntrospectionAnchorID) -> Target?) {
-        self.selector = selector
+    private var receiverSelector: (IntrospectionPlatformViewController, IntrospectionAnchorID) -> Target?
+    private var ancestorSelector: (IntrospectionPlatformViewController) -> Target?
+
+    private init(
+        receiverSelector: @escaping (IntrospectionPlatformViewController, IntrospectionAnchorID) -> Target?,
+        ancestorSelector: @escaping (IntrospectionPlatformViewController) -> Target?
+    ) {
+        self.receiverSelector = receiverSelector
+        self.ancestorSelector = ancestorSelector
+    }
+
+    @_spi(Internals)
+    public func withReceiverSelector(_ selector: @escaping (PlatformViewController, IntrospectionAnchorID) -> Target?) -> Self {
+        var copy = self
+        copy.receiverSelector = selector
+        return copy
+    }
+
+    @_spi(Internals)
+    public func withAncestorSelector(_ selector: @escaping (PlatformViewController) -> Target?) -> Self {
+        var copy = self
+        copy.ancestorSelector = selector
+        return copy
     }
 
     func callAsFunction(
@@ -52,6 +45,33 @@ public struct IntrospectionSelector<Target: PlatformEntity> {
         _ scope: IntrospectionScope,
         _ anchorID: IntrospectionAnchorID
     ) -> Target? {
-        selector(controller, scope, anchorID)
+        if
+            scope.contains(.receiver),
+            let target = receiverSelector(controller, anchorID)
+        {
+            return target
+        }
+        if
+            scope.contains(.ancestor),
+            let target = ancestorSelector(controller)
+        {
+            return target
+        }
+        return nil
+    }
+}
+
+extension PlatformViewController {
+    func `as`<Entity: PlatformEntity>(_ entityType: Entity.Type) -> (any PlatformEntity)? {
+        if Entity.Base.self == PlatformView.self {
+            #if canImport(UIKit)
+            return viewIfLoaded
+            #elseif canImport(AppKit)
+            return isViewLoaded ? view : nil
+            #endif
+        } else if Entity.Base.self == PlatformViewController.self {
+            return self
+        }
+        return nil
     }
 }

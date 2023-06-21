828705f90187200c0bde5ef8e800fc9f0d8cfb41
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-21 15:53:41 +0100

    wip

--- Sources/ViewTypes/ProgressViewWithLinearStyle.swift
+++ Sources/ViewTypes/ProgressViewWithLinearStyle.swift
@@ -1,7 +1,24 @@
 import SwiftUI
 
-// MARK: SwiftUI.ProgressView().progressViewStyle(.linear)
-
+/// An abstract representation of the `ProgressView` type in SwiftUI, with `.linear` style.
+///
+/// ```swift
+/// struct ContentView: View {
+///     var body: some View {
+///         ProgressView(value: 0.5)
+///             .progressViewStyle(.linear)
+///             #if os(iOS) || os(tvOS)
+///             .introspect(.progressView(style: .linear), on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17)) {
+///                 print(type(of: $0)) // UIProgressView
+///             }
+///             #elseif os(macOS)
+///             .introspect(.progressView(style: .linear), on: .macOS(.v11, .v12, .v13, .v14)) {
+///                 print(type(of: $0)) // NSProgressIndicator
+///             }
+///             #endif
+///     }
+/// }
+/// ```
 public struct ProgressViewWithLinearStyleType: IntrospectableViewType {
     public enum Style {
         case linear

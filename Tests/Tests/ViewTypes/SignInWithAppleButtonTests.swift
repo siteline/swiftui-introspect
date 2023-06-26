3cfdd8baf035615f83be358c4786fdec0c55d977
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-26 18:16:19 +0100

    Add `SignInWithAppleButton` introspection (#265)

---
+++ Tests/Tests/ViewTypes/SignInWithAppleButtonTests.swift
@@ -0,0 +1,50 @@
+#if canImport(AuthenticationServices)
+import AuthenticationServices
+import SwiftUI
+import SwiftUIIntrospect
+import XCTest
+
+@available(iOS 14, tvOS 14, macOS 11, *)
+final class SignInWithAppleButtonTests: XCTestCase {
+    typealias PlatformSignInWithAppleButton = ASAuthorizationAppleIDButton
+
+    func testSignInWithAppleButton() throws {
+        guard #available(iOS 14, tvOS 14, macOS 11, *) else {
+            throw XCTSkip()
+        }
+
+        XCTAssertViewIntrospection(of: PlatformSignInWithAppleButton.self) { spies in
+            let spy0 = spies[0]
+            let spy1 = spies[1]
+            let spy2 = spies[2]
+
+            VStack {
+                SignInWithAppleButton(.continue, onRequest: { _ in }, onCompletion: { _ in })
+                    .introspect(
+                        .signInWithAppleButton,
+                        on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), .macOS(.v11, .v12, .v13, .v14),
+                        customize: spy0
+                    )
+
+                SignInWithAppleButton(.signIn, onRequest: { _ in }, onCompletion: { _ in })
+                    .introspect(
+                        .signInWithAppleButton,
+                        on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), .macOS(.v11, .v12, .v13, .v14),
+                        customize: spy1
+                    )
+
+                SignInWithAppleButton(.signUp, onRequest: { _ in }, onCompletion: { _ in })
+                    .introspect(
+                        .signInWithAppleButton,
+                        on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), .macOS(.v11, .v12, .v13, .v14),
+                        customize: spy2
+                    )
+            }
+        } extraAssertions: {
+            XCTAssertNotIdentical($0[safe: 0], $0[safe: 1])
+            XCTAssertNotIdentical($0[safe: 0], $0[safe: 2])
+            XCTAssertNotIdentical($0[safe: 1], $0[safe: 2])
+        }
+    }
+}
+#endif

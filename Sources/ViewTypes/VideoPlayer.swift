ebc92dfb2e4d94b54a0158d4ac09052c7885907f
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-26 14:33:56 +0100

    wip

--- Sources/ViewTypes/VideoPlayer.swift
+++ Sources/ViewTypes/VideoPlayer.swift
@@ -4,7 +4,6 @@ import SwiftUI
 
 public struct VideoPlayerType: IntrospectableViewType {}
 
-#if !os(tvOS)
 extension IntrospectableViewType where Self == VideoPlayerType {
     public static var videoPlayer: Self { .init() }
 }
@@ -18,14 +17,23 @@ extension iOSViewVersion<VideoPlayerType, AVPlayerViewController> {
     public static let v16 = Self(for: .v16)
     public static let v17 = Self(for: .v17)
 }
+
+extension tvOSViewVersion<VideoPlayerType, AVPlayerViewController> {
+    @available(*, unavailable, message: "VideoPlayer isn't available on tvOS 13")
+    public static let v13 = Self.unavailable()
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+    public static let v17 = Self(for: .v17)
+}
 #elseif canImport(AppKit)
-//extension macOSViewVersion<VideoPlayerType, NSSwitch> {
-//    public static let v10_15 = Self(for: .v10_15)
-//    public static let v11 = Self(for: .v11)
-//    public static let v12 = Self(for: .v12)
-//    public static let v13 = Self(for: .v13)
-//    public static let v14 = Self(for: .v14)
-//}
-#endif
+extension macOSViewVersion<VideoPlayerType, AVPlayerView> {
+    @available(*, unavailable, message: "VideoPlayer isn't available on macOS 10.15")
+    public static let v10_15 = Self(for: .v10_15)
+    public static let v11 = Self(for: .v11)
+    public static let v12 = Self(for: .v12)
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+}
 #endif
 #endif
--- Tests/Tests/ViewTypes/VideoPlayerTests.swift
+++ Tests/Tests/ViewTypes/VideoPlayerTests.swift
@@ -4,20 +4,22 @@ import SwiftUI
 import SwiftUIIntrospect
 import XCTest
 
-@available(iOS 14, macOS 11, *)
+@available(iOS 14, tvOS 14, macOS 11, *)
 final class VideoPlayerTests: XCTestCase {
     #if canImport(UIKit)
     typealias PlatformVideoPlayer = AVPlayerViewController
     #elseif canImport(AppKit)
-    typealias PlatformVideoPlayer = NSColorWell
+    typealias PlatformVideoPlayer = AVPlayerView
     #endif
 
     func testVideoPlayer() throws {
-        guard #available(iOS 14, macOS 11, *) else {
+        guard #available(iOS 14, tvOS 14, macOS 11, *) else {
             throw XCTSkip()
         }
 
-        let videoURL = URL(string: "https://bit.ly/swswift")!
+        let videoURL0 = URL(string: "https://bit.ly/swswift#1")!
+        let videoURL1 = URL(string: "https://bit.ly/swswift#2")!
+        let videoURL2 = URL(string: "https://bit.ly/swswift#3")!
 
         XCTAssertViewIntrospection(of: PlatformVideoPlayer.self) { spies in
             let spy0 = spies[0]
@@ -25,39 +27,32 @@ final class VideoPlayerTests: XCTestCase {
             let spy2 = spies[2]
 
             VStack {
-                VideoPlayer(player: AVPlayer(url: videoURL))
-                    #if os(iOS)
-                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), customize: spy0)
-//                    #elseif os(macOS)
-//                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy0)
+                VideoPlayer(player: AVPlayer(url: videoURL0))
+                    #if os(iOS) || os(tvOS)
+                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), customize: spy0)
+                    #elseif os(macOS)
+                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy0)
                     #endif
 
-                VideoPlayer(player: AVPlayer(url: videoURL))
-                    #if os(iOS)
-                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), customize: spy1)
-//                    #elseif os(macOS)
-//                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy1)
+                VideoPlayer(player: AVPlayer(url: videoURL1))
+                    #if os(iOS) || os(tvOS)
+                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), customize: spy1)
+                    #elseif os(macOS)
+                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy1)
                     #endif
 
-                VideoPlayer(player: AVPlayer(url: videoURL))
-                    #if os(iOS)
-                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), customize: spy2)
-//                    #elseif os(macOS)
-//                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy2)
+                VideoPlayer(player: AVPlayer(url: videoURL2))
+                    #if os(iOS) || os(tvOS)
+                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17), customize: spy2)
+                    #elseif os(macOS)
+                    .introspect(.videoPlayer, on: .macOS(.v11, .v12, .v13, .v14), customize: spy2)
                     #endif
             }
+        } extraAssertions: {
+            XCTAssertEqual(($0[safe: 0]?.player?.currentItem?.asset as? AVURLAsset)?.url, videoURL0)
+            XCTAssertEqual(($0[safe: 1]?.player?.currentItem?.asset as? AVURLAsset)?.url, videoURL1)
+            XCTAssertEqual(($0[safe: 2]?.player?.currentItem?.asset as? AVURLAsset)?.url, videoURL2)
         }
-//    extraAssertions: {
-//            #if canImport(UIKit)
-//            XCTAssertEqual($0[safe: 0]?.selectedColor, .red)
-//            XCTAssertEqual($0[safe: 1]?.selectedColor, .green)
-//            XCTAssertEqual($0[safe: 2]?.selectedColor, .blue)
-//            #elseif canImport(AppKit)
-//            XCTAssertEqual($0[safe: 0]?.color, .red)
-//            XCTAssertEqual($0[safe: 1]?.color, .green)
-//            XCTAssertEqual($0[safe: 2]?.color, .blue)
-//            #endif
-//        }
     }
 }
 #endif

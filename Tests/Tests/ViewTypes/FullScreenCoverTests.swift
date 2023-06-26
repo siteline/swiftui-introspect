a570ded6aa25f133477e51581fb39a4b0adb5767
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-26 22:58:11 +0100

    wip

--- Tests/Tests/ViewTypes/FullScreenCoverTests.swift
+++ Tests/Tests/ViewTypes/FullScreenCoverTests.swift
@@ -4,23 +4,24 @@ import SwiftUIIntrospect
 import XCTest
 
 final class FullScreenCoverTests: XCTestCase {
-    // FIXME: this doesn't pass, even though introspection works in the Showcase app
-//    func testPresentationAsFullScreenCover() throws {
-//        XCTAssertViewIntrospection(of: UIPresentationController.self) { spies in
-//            let spy0 = spies[0]
-//
-//            Text("Root")
-//                .fullScreenCover(isPresented: .constant(true)) {
-//                    Text("Content")
-//                        #if os(iOS) || os(tvOS)
-//                        .introspect(
-//                            .fullScreenCover,
-//                            on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17),
-//                            customize: spy0
-//                        )
-//                        #endif
-//                }
-//        }
-//    }
+    func testPresentationAsFullScreenCover() throws {
+        throw XCTSkip("FIXME: this doesn't pass, even though introspection works in the Showcase app")
+
+        XCTAssertViewIntrospection(of: UIPresentationController.self) { spies in
+            let spy0 = spies[0]
+
+            Text("Root")
+                .fullScreenCover(isPresented: .constant(true)) {
+                    Text("Content")
+                        #if os(iOS) || os(tvOS)
+                        .introspect(
+                            .fullScreenCover,
+                            on: .iOS(.v14, .v15, .v16, .v17), .tvOS(.v14, .v15, .v16, .v17),
+                            customize: spy0
+                        )
+                        #endif
+                }
+        }
+    }
 }
 #endif

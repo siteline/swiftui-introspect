01466b8f078d2d4f46d5dc31011e3d4051e7af38
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-27 13:24:29 +0100

    wip

--- Sources/IntrospectableViewType.swift
+++ Sources/IntrospectableViewType.swift
@@ -1,4 +1,11 @@
 public protocol IntrospectableViewType {
+    /// The scope of introspection for this particular view type.
+    ///
+    /// While the scope can be overridden by the user in their `introspect` call,
+    /// most of the time it's preferable to use the view's own.
+    ///
+    /// Defaults to `.receiver` if left unimplemented, which is a sensible one in
+    /// most cases.
     var scope: IntrospectionScope { get }
 }
 

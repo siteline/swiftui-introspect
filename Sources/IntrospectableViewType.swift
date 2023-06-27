01546f81bce2ad91ee1f3f4503dc1710b51b0e3a
David Roman <2538074+davdroman@users.noreply.github.com>
2023-06-27 13:31:09 +0100

    wip

--- Sources/IntrospectableViewType.swift
+++ Sources/IntrospectableViewType.swift
@@ -1,11 +1,14 @@
 public protocol IntrospectableViewType {
-    /// The scope of introspection for this particular view type.
+    /// The scope of introspection for this particular view type, i.e. where introspect
+    /// should look to find the desired target view relative to the applied
+    /// `.introspect(...)` modifier.
     ///
-    /// While the scope can be overridden by the user in their `introspect` call,
-    /// most of the time it's preferable to use the view's own.
+    /// While the scope can be overridden by the user in their `.introspect(...)` call,
+    /// most of the time it's preferable to defer to the view type's own scope,
+    /// as it guarantees introspection is working as intended by the vendor.
     ///
     /// Defaults to `.receiver` if left unimplemented, which is a sensible one in
-    /// most cases.
+    /// most cases if you're looking to implement your own view type.
     var scope: IntrospectionScope { get }
 }
 

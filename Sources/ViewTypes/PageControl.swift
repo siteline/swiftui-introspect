f26c40063ab988b0a356529a168f355c2ea6d2fc
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-05 21:58:57 +0100

    wip

---
+++ Sources/ViewTypes/PageControl.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+/// An abstract representation of the page control type in SwiftUI.
+///
+/// ### iOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     var body: some View {
+///         TabView {
+///             Text("Page 1").frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+///             Text("Page 2").frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
+///         }
+///         .tabViewStyle(.page(indexDisplayMode: .always))
+///         .introspect(.pageControl, on: .iOS(.v14, .v15, .v16, .v17)) {
+///             print(type(of: $0)) // UIPageControl
+///         }
+///     }
+/// }
+/// ```
+///
+/// ### tvOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     var body: some View {
+///         TabView {
+///             Text("Page 1").frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+///             Text("Page 2").frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
+///         }
+///         .tabViewStyle(.page(indexDisplayMode: .always))
+///         .introspect(.pageControl, on: .tvOS(.v14, .v15, .v16, .v17)) {
+///             print(type(of: $0)) // UIPageControl
+///         }
+///     }
+/// }
+/// ```
+///
+/// ### macOS
+///
+/// Not available.
+///
+public struct PageControlType: IntrospectableViewType {}
+
+extension IntrospectableViewType where Self == PageControlType {
+    public static var pageControl: Self { .init() }
+}
+
+#if canImport(UIKit)
+extension iOSViewVersion<PageControlType, UIPageControl> {
+    @available(*, unavailable, message: "TabView {}.tabViewStyle(.page) isn't available on iOS 13")
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+    public static let v17 = Self(for: .v17)
+}
+
+extension tvOSViewVersion<PageControlType, UIPageControl> {
+    @available(*, unavailable, message: "TabView {}.tabViewStyle(.page) isn't available on tvOS 13")
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+    public static let v17 = Self(for: .v17)
+}
+#endif

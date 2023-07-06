2cb404d17d044995fe644b2e9fc020af75bf54ba
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-06 19:21:08 +0100

    Gardening [skip ci]

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -10,12 +10,12 @@ let package = Package(
         .macOS(.v10_15),
     ],
     products: [
-        // legacy library
+        // old module
         .library(name: "Introspect", targets: ["Introspect"]),
         .library(name: "Introspect-Static", type: .static, targets: ["Introspect"]),
         .library(name: "Introspect-Dynamic", type: .dynamic, targets: ["Introspect"]),
 
-        // new experimental library
+        // new module
         .library(name: "SwiftUIIntrospect", targets: ["SwiftUIIntrospect"]),
         .library(name: "SwiftUIIntrospect-Static", type: .static, targets: ["SwiftUIIntrospect"]),
         .library(name: "SwiftUIIntrospect-Dynamic", type: .dynamic, targets: ["SwiftUIIntrospect"]),

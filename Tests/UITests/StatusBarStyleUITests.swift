a4d8959fa19d3b9acbd4df894984716287b9c50b
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-18 17:48:24 +0100

    wip

---
+++ Tests/UITests/StatusBarStyleUITests.swift
@@ -0,0 +1,12 @@
+import UITestsHostApp
+import XCTest
+
+final class StatusBarStyleUITests: UITestCase {
+    override var testCase: TestCase {
+        .statusBarStyle
+    }
+
+    func test() {
+        
+    }
+}
---
+++ Tests/UITests/UITestCase.swift
@@ -0,0 +1,22 @@
+import SimulatorStatusMagic
+import UITestsHostApp
+import XCTest
+
+class UITestCase: XCTestCase {
+    var testCase: TestCase {
+        preconditionFailure("Please override this property")
+    }
+    
+    let app = XCUIApplication()
+    
+    override func invokeTest() {
+        SDStatusBarManager.sharedInstance().enableOverrides()
+        
+        continueAfterFailure = false
+        
+        app.launchEnvironment["testCase"] = testCase.rawValue
+        app.launch()
+        
+        super.invokeTest()
+    }
+}

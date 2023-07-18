857fc69f79b123f5a0d6bfbdd74aa7d0ee252bed
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-18 18:48:10 +0100

    wip

--- Tests/UITests/StatusBarStyleUITests.swift
+++ Tests/UITests/StatusBarStyleUITests.swift
@@ -1,3 +1,4 @@
+import SnapshotTesting
 import XCTest
 
 final class StatusBarStyleUITests: UITestCase {
@@ -9,5 +10,12 @@ final class StatusBarStyleUITests: UITestCase {
         guard #unavailable(iOS 17) else {
             throw XCTSkip("SimulatorStatusMagic stopped working in iOS 17, so we can no longer consistently compare status bar screenshots")
         }
+
+        app.buttons["Navigate To Detail (bugged)"].tap()
+        app.buttons["Navigate To Detail"].tap()
+        app.buttons["Navigate To Detail"].tap()
+        app.buttons["Navigate Back"].tap()
+
+        assertSnapshot(matching: app.windows.firstMatch.screenshot().image, as: .image)
     }
 }

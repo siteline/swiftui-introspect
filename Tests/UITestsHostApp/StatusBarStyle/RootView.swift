cf02def6b56c56828e6c34edbf8905cb213f8450
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-19 21:25:52 +0100

    Add UI Test suite (#314)

---
+++ Tests/UITestsHostApp/StatusBarStyle/RootView.swift
@@ -0,0 +1,52 @@
+//
+//  RootView.swift
+//  Showcase
+//
+//  Created by Orackle on 14.07.2023.
+//
+
+import SwiftUI
+import SwiftUIIntrospect
+
+struct RootView: View {
+
+    var body: some View {
+        VStack {
+            Button("Navigate To Detail", action: navigateToDetail)
+        }
+    }
+
+    private func navigateToDetail() {
+        let controller = HostingController(rootView: DetailView())
+        controller.statusBarStyle = .lightContent
+        NavigationController.shared?.pushViewController(controller, animated: true)
+    }
+}
+
+struct DetailView: View {
+    @Environment(\.presentationMode) var dismiss
+
+    var body: some View {
+        ZStack {
+            Color.red.edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Button("Navigate To Detail", action: navigateToDetail)
+                Button("Navigate Back", action: goBack)
+            }
+        }
+        .introspect(.viewController, on: .iOS(.v13, .v14, .v15, .v16)) { viewController in
+            /// some customizations there
+        }
+    }
+
+    private func goBack() {
+        dismiss.wrappedValue.dismiss()
+    }
+
+    private func navigateToDetail() {
+        let controller = HostingController(rootView: DetailView())
+        controller.statusBarStyle = .lightContent
+        NavigationController.shared?.pushViewController(controller, animated: true)
+    }
+}

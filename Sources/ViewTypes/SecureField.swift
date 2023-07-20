22d75759bbab73061a23724d921afbcaa07cbbe6
David Roman <2538074+davdroman@users.noreply.github.com>
2023-07-20 14:50:39 +0100

    Add `SecureField` introspection (#317)

---
+++ Sources/ViewTypes/SecureField.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+
+/// An abstract representation of the `SecureField` type in SwiftUI.
+///
+/// ### iOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     @State var text = "Lorem ipsum"
+///
+///     var body: some View {
+///         SecureField("Secure Field", text: $text)
+///             .introspect(.secureField, on: .iOS(.v13, .v14, .v15, .v16, .v17)) {
+///                 print(type(of: $0)) // UISecureField
+///             }
+///     }
+/// }
+/// ```
+///
+/// ### tvOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     @State var text = "Lorem ipsum"
+///
+///     var body: some View {
+///         SecureField("Secure Field", text: $text)
+///             .introspect(.secureField, on: .tvOS(.v13, .v14, .v15, .v16, .v17)) {
+///                 print(type(of: $0)) // UISecureField
+///             }
+///     }
+/// }
+/// ```
+///
+/// ### macOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     @State var text = "Lorem ipsum"
+///
+///     var body: some View {
+///         SecureField("Secure Field", text: $text)
+///             .introspect(.secureField, on: .macOS(.v10_15, .v11, .v12, .v13, .v14)) {
+///                 print(type(of: $0)) // NSSecureField
+///             }
+///     }
+/// }
+/// ```
+public struct SecureFieldType: IntrospectableViewType {}
+
+extension IntrospectableViewType where Self == SecureFieldType {
+    public static var secureField: Self { .init() }
+}
+
+#if canImport(UIKit)
+extension iOSViewVersion<SecureFieldType, UITextField> {
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+    public static let v17 = Self(for: .v17)
+}
+
+extension tvOSViewVersion<SecureFieldType, UITextField> {
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+    public static let v15 = Self(for: .v15)
+    public static let v16 = Self(for: .v16)
+    public static let v17 = Self(for: .v17)
+}
+#elseif canImport(AppKit)
+extension macOSViewVersion<SecureFieldType, NSTextField> {
+    public static let v10_15 = Self(for: .v10_15)
+    public static let v11 = Self(for: .v11)
+    public static let v12 = Self(for: .v12)
+    public static let v13 = Self(for: .v13)
+    public static let v14 = Self(for: .v14)
+}
+#endif

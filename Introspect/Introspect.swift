9b721dd15de5888312122ae4f202c7a76daba809
Larry <larrytran555@gmail.com>
2023-07-25 10:41:58 -0700

    Adds findChildUsingFrame

--- Introspect/Introspect.swift
+++ Introspect/Introspect.swift
@@ -199,13 +199,46 @@ public enum Introspect {
     public static func findAncestor<AnyViewType: PlatformView>(ofType type: AnyViewType.Type, from entry: PlatformView) -> AnyViewType? {
         var superview = entry.superview
         while let s = superview {
-            if let typed = s as? AnyViewType {
+            if let typed = s as? AnyViewType ?? findChildUsingFrame(ofType: type, in: s, from: entry) {
                 return typed
             }
             superview = s.superview
         }
         return nil
     }
+
+    /// Finds a subview of the specified type.
+        /// This method will recursively look for this view.
+        /// Returns nil if it can't find a view of the specified type.
+        public static func findChildUsingFrame<AnyViewType: PlatformView>(
+            ofType type: AnyViewType.Type,
+            in root: PlatformView,
+            from originalEntry: PlatformView
+        ) -> AnyViewType? {
+            var children: [AnyViewType] = []
+            for subview in root.subviews {
+                if let typed = subview as? AnyViewType {
+                    children.append(typed)
+                } else if let typed = findChild(ofType: type, in: subview) {
+                    children.append(typed)
+                }
+            }
+
+            if children.count > 1 {
+                for child in children {
+                    let converted = child.convert(
+                        CGPoint(x: originalEntry.frame.size.width / 2, y: originalEntry.frame.size.height / 2),
+                        from: originalEntry
+                    )
+                    if CGRect(origin: .zero, size: child.frame.size).contains(converted) {
+                        return child
+                    }
+                }
+                return nil
+            }
+
+            return children.first
+        }
     
     /// Finds the hosting view of a specific subview.
     /// Hosting views generally contain subviews for one specific SwiftUI element.

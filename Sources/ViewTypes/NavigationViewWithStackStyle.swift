dd82f4e92a211900d30fabc05f5a7b8b0335d1d9
David Roman <2538074+davdroman@users.noreply.github.com>
2023-08-10 18:06:22 +0100

    wip

--- Sources/ViewTypes/NavigationViewWithStackStyle.swift
+++ Sources/ViewTypes/NavigationViewWithStackStyle.swift
@@ -38,6 +38,21 @@ import SwiftUI
 ///
 /// Not available.
 ///
+/// ### visionOS
+///
+/// ```swift
+/// struct ContentView: View {
+///     var body: some View {
+///         NavigationView {
+///             Text("Root")
+///         }
+///         .navigationViewStyle(.stack)
+///         .introspect(.navigationView(style: .stack), on: .visionOS(.v1)) {
+///             print(type(of: $0)) // UINavigationController
+///         }
+///     }
+/// }
+/// ```
 public struct NavigationViewWithStackStyleType: IntrospectableViewType {
     public enum Style {
         case stack
@@ -67,6 +82,14 @@ extension tvOSViewVersion<NavigationViewWithStackStyleType, UINavigationControll
     public static let v15 = Self(for: .v15, selector: selector)
     public static let v16 = Self(for: .v16, selector: selector)
     public static let v17 = Self(for: .v17, selector: selector)
+
+    private static var selector: IntrospectionSelector<UINavigationController> {
+        .default.withAncestorSelector(\.navigationController)
+    }
+}
+
+extension visionOSViewVersion<NavigationViewWithStackStyleType, UINavigationController> {
+    public static let v1 = Self(for: .v1, selector: selector)
 
     private static var selector: IntrospectionSelector<UINavigationController> {
         .default.withAncestorSelector(\.navigationController)

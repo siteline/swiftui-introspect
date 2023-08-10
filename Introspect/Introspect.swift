6e39176a7cebf7b2b15c66eb1ed8bbf0d3ecd4e0
David Roman <2538074+davdroman@users.noreply.github.com>
2023-08-10 19:41:13 +0100

    Revert "Add visionOS as one of the platforms to check for."
    This reverts commit 77964d1b5d7f8939d28a448c47e34943874e675e.

--- Introspect/Introspect.swift
+++ Introspect/Introspect.swift
@@ -3,14 +3,14 @@ import SwiftUI
 #if os(macOS)
 public typealias PlatformView = NSView
 #endif
-#if os(iOS) || os(tvOS) || os(visionOS)
+#if os(iOS) || os(tvOS)
 public typealias PlatformView = UIView
 #endif
 
 #if os(macOS)
 public typealias PlatformViewController = NSViewController
 #endif
-#if os(iOS) || os(tvOS) || os(visionOS)
+#if os(iOS) || os(tvOS)
 public typealias PlatformViewController = UIViewController
 #endif
 

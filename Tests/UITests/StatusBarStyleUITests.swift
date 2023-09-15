b0c101798171a4d09ff7f903a155dd6a032facca
David Roman <2538074+davdroman@users.noreply.github.com>
2023-09-15 16:53:36 +0100

    wip

--- Tests/UITests/StatusBarStyleUITests.swift
+++ Tests/UITests/StatusBarStyleUITests.swift
@@ -7,10 +7,6 @@ final class StatusBarStyleUITests: UITestCase {
     }
 
     func test() throws {
-        guard #unavailable(iOS 17) else {
-            throw XCTSkip("SimulatorStatusMagic stopped working in iOS 17, so we can no longer consistently compare status bar screenshots")
-        }
-
         app.buttons["Navigate To Detail"].tap()
         app.buttons["Navigate To Detail"].tap()
         app.buttons["Navigate Back"].tap()

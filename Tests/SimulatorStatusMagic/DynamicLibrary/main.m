a7b440a7bda8b0799608381141f4f900c91f7198
David Roman <2538074+davdroman@users.noreply.github.com>
2023-09-15 17:02:18 +0100

    wip

--- Tests/SimulatorStatusMagic/DynamicLibrary/main.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <OSLog/OSLog.h>
-#import "SDStatusBarManager.h"
-
-__attribute__((constructor))
-static void pluginDidStart(void) {
-  [SDStatusBarManager.sharedInstance enableOverrides];
-}
--- Tests/SimulatorStatusMagic/build_and_inject.sh
+++
@@ -1,3 +0,0 @@
-xcodebuild -scheme "SimulatorStatusMagicDlib" -sdk iphonesimulator -derivedDataPath build
-xcrun simctl spawn $1 launchctl debug system/com.apple.SpringBoard --environment DYLD_INSERT_LIBRARIES="$PWD/build/Build/Products/Debug-iphonesimulator/libSimulatorStatusMagicDlib.dylib"
-xcrun simctl spawn $1 launchctl stop com.apple.SpringBoard

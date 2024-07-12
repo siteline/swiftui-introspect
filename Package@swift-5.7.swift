c4625c2f167832757baae4b4abb99c864272b949
Alexis Schultz <Alex293@users.noreply.github.com>
2024-07-12 10:36:13 +0200

    [Swift 6] Update Package description for Swift 6 and 5 support in Xcode16

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -19,6 +19,5 @@ let package = Package(
             name: "SwiftUIIntrospect",
             path: "Sources"
         ),
-    ],
-    swiftLanguageVersions: [.v5, .v6]
+    ]
 )

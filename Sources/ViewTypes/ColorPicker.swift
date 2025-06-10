70d1ea8bce486e72423163e78f46b76277a5e084
David Roman <2538074+davdroman@users.noreply.github.com>
2025-06-10 17:49:24 +0100

    WIP

--- Sources/ViewTypes/ColorPicker.swift
+++ Sources/ViewTypes/ColorPicker.swift
@@ -11,7 +11,7 @@ import SwiftUI
 ///
 ///     var body: some View {
 ///         ColorPicker("Pick a color", selection: $color)
-///             .introspect(.colorPicker, on: .iOS(.v14, .v15, .v16, .v17, .v18)) {
+///             .introspect(.colorPicker, on: .iOS(.v14, .v15, .v16, .v17, .v18, .v26)) {
 ///                 print(type(of: $0)) // UIColorPicker
 ///             }
 ///     }
@@ -30,7 +30,7 @@ import SwiftUI
 ///
 ///     var body: some View {
 ///         ColorPicker("Pick a color", selection: $color)
-///             .introspect(.colorPicker, on: .macOS(.v11, .v12, .v13, .v14, .v15)) {
+///             .introspect(.colorPicker, on: .macOS(.v11, .v12, .v13, .v14, .v15, .v26)) {
 ///                 print(type(of: $0)) // NSColorPicker
 ///             }
 ///     }
@@ -45,7 +45,7 @@ import SwiftUI
 ///
 ///     var body: some View {
 ///         ColorPicker("Pick a color", selection: $color)
-///             .introspect(.colorPicker, on: .visionOS(.v1, .v2)) {
+///             .introspect(.colorPicker, on: .visionOS(.v1, .v2, .v26)) {
 ///                 print(type(of: $0)) // UIColorPicker
 ///             }
 ///     }
@@ -68,12 +68,14 @@ extension iOSViewVersion<ColorPickerType, UIColorWell> {
     public static let v16 = Self(for: .v16)
     public static let v17 = Self(for: .v17)
     public static let v18 = Self(for: .v18)
+    public static let v26 = Self(for: .v26)
 }
 
 @available(iOS 14, *)
 extension visionOSViewVersion<ColorPickerType, UIColorWell> {
     public static let v1 = Self(for: .v1)
     public static let v2 = Self(for: .v2)
+    public static let v26 = Self(for: .v26)
 }
 #elseif canImport(AppKit)
 @available(macOS 11, *)
@@ -85,6 +87,7 @@ extension macOSViewVersion<ColorPickerType, NSColorWell> {
     public static let v13 = Self(for: .v13)
     public static let v14 = Self(for: .v14)
     public static let v15 = Self(for: .v15)
+    public static let v26 = Self(for: .v26)
 }
 #endif
 #endif

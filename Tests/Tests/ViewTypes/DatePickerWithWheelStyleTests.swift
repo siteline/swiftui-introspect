d5ef70546ca73d2026accd44aeef57354bc65915
David Roman <2538074+davdroman@users.noreply.github.com>
2025-07-09 18:57:54 +0100

    WIP

--- Tests/Tests/ViewTypes/DatePickerWithWheelStyleTests.swift
+++ Tests/Tests/ViewTypes/DatePickerWithWheelStyleTests.swift
@@ -1,32 +1,22 @@
 #if !os(tvOS) && !os(macOS)
 import SwiftUI
 import SwiftUIIntrospect
-import XCTest
+import Testing
 
 @MainActor
-final class DatePickerWithWheelStyleTests: XCTestCase {
+@Suite
+struct DatePickerWithWheelStyleTests {
     #if canImport(UIKit)
     typealias PlatformDatePickerWithWheelStyle = UIDatePicker
     #endif
 
-    func testDatePickerWithWheelStyle() {
-        let date0 = Date(timeIntervalSince1970: 0)
-        let date1 = Date(timeIntervalSince1970: 5)
-        let date2 = Date(timeIntervalSince1970: 10)
+    @Test func introspect() async throws {
+        let date1 = Date(timeIntervalSince1970: 0)
+        let date2 = Date(timeIntervalSince1970: 5)
+        let date3 = Date(timeIntervalSince1970: 10)
 
-        XCTAssertViewIntrospection(of: PlatformDatePickerWithWheelStyle.self) { spies in
-            let spy0 = spies[0]
-            let spy1 = spies[1]
-            let spy2 = spies[2]
-
+        let (entity1, entity2, entity3) = try await introspection(of: PlatformDatePickerWithWheelStyle.self) { spy1, spy2, spy3 in
             VStack {
-                DatePicker("", selection: .constant(date0))
-                    .datePickerStyle(.wheel)
-                    #if os(iOS) || os(visionOS)
-                    .introspect(.datePicker(style: .wheel), on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .visionOS(.v1, .v2, .v26), customize: spy0)
-                    #endif
-                    .cornerRadius(8)
-
                 DatePicker("", selection: .constant(date1))
                     .datePickerStyle(.wheel)
                     #if os(iOS) || os(visionOS)
@@ -39,14 +29,20 @@ final class DatePickerWithWheelStyleTests: XCTestCase {
                     #if os(iOS) || os(visionOS)
                     .introspect(.datePicker(style: .wheel), on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .visionOS(.v1, .v2, .v26), customize: spy2)
                     #endif
+                    .cornerRadius(8)
+
+                DatePicker("", selection: .constant(date3))
+                    .datePickerStyle(.wheel)
+                    #if os(iOS) || os(visionOS)
+                    .introspect(.datePicker(style: .wheel), on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .visionOS(.v1, .v2, .v26), customize: spy3)
+                    #endif
             }
-        } extraAssertions: {
-            #if canImport(UIKit)
-            XCTAssertEqual($0[safe: 0]?.date, date0)
-            XCTAssertEqual($0[safe: 1]?.date, date1)
-            XCTAssertEqual($0[safe: 2]?.date, date2)
-            #endif
         }
+        #if canImport(UIKit)
+        #expect(entity1.date == date1)
+        #expect(entity2.date == date2)
+        #expect(entity3.date == date3)
+        #endif
     }
 }
 #endif

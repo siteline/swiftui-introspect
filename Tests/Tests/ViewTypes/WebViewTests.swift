e1b81ca9ff636ca9b0ac685f5893723025eeaec7
David Roman <2538074+davdroman@users.noreply.github.com>
2025-07-09 18:33:23 +0100

    WIP

--- Tests/Tests/ViewTypes/WebViewTests.swift
+++ Tests/Tests/ViewTypes/WebViewTests.swift
@@ -8,20 +8,9 @@ import WebKit
 @Suite
 struct WebViewTests {
     @available(iOS 26, tvOS 26, macOS 26, visionOS 26, *)
-    @Test func webView() async throws {
-        XCTAssertViewIntrospection(of: WKWebView.self) { spies in
-            let spy0 = spies[0]
-            let spy1 = spies[1]
-            let spy2 = spies[2]
-
+    @Test func introspect() async throws {
+        let (entity1, entity2, entity3) = try await introspection(of: WKWebView.self) { spy1, spy2, spy3 in
             VStack {
-                WebView(url: nil)
-                    .introspect(
-                        .webView,
-                        on: .iOS(.v26), .tvOS(.v26), .macOS(.v26), .visionOS(.v26),
-                        customize: spy0
-                    )
-
                 WebView(url: nil)
                     .introspect(
                         .webView,
@@ -35,12 +24,18 @@ struct WebViewTests {
                         on: .iOS(.v26), .tvOS(.v26), .macOS(.v26), .visionOS(.v26),
                         customize: spy2
                     )
+
+                WebView(url: nil)
+                    .introspect(
+                        .webView,
+                        on: .iOS(.v26), .tvOS(.v26), .macOS(.v26), .visionOS(.v26),
+                        customize: spy3
+                    )
             }
-        } extraAssertions: {
-            #expect($0[safe: 0] !== $0[safe: 1])
-            #expect($0[safe: 0] !== $0[safe: 2])
-            #expect($0[safe: 1] !== $0[safe: 2])
         }
+        #expect(entity1 !== entity2)
+        #expect(entity1 !== entity3)
+        #expect(entity2 !== entity3)
     }
 }
 #endif

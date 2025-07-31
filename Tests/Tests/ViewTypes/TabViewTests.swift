d32609e9e29cbb1350659b77b5b7d18e985d5ec3
David Roman <2538074+davdroman@users.noreply.github.com>
2025-07-31 15:27:51 +0100

    WIP

--- Tests/Tests/ViewTypes/TabViewTests.swift
+++ Tests/Tests/ViewTypes/TabViewTests.swift
@@ -45,5 +45,41 @@ struct TabViewTests {
             }
         }
     }
+
+    @Test func introspectWithNonRootPlacement() async throws {
+        try await introspection(of: PlatformTabView.self) { spy in
+            GroupBox {
+                TabView {
+                    ZStack {
+                        Color.red
+                        Text("Something")
+                    }
+                }
+                #if os(iOS) || os(tvOS)
+                .introspect(.tabView, on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .tvOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), customize: spy)
+                #elseif os(macOS)
+                .introspect(.tabView, on: .macOS(.v10_15, .v11, .v12, .v13, .v14, .v15, .v26), customize: spy)
+                #endif
+            }
+        }
+    }
+
+    @Test func introspectWithNonRootPlacementAsAncestor() async throws {
+        try await introspection(of: PlatformTabView.self) { spy in
+            GroupBox {
+                TabView {
+                    ZStack {
+                        Color.red
+                        Text("Something")
+                            #if os(iOS) || os(tvOS)
+                            .introspect(.tabView, on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .tvOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), scope: .ancestor, customize: spy)
+                            #elseif os(macOS)
+                            .introspect(.tabView, on: .macOS(.v10_15, .v11, .v12, .v13, .v14, .v15, .v26), scope: .ancestor, customize: spy)
+                            #endif
+                    }
+                }
+            }
+        }
+    }
 }
 #endif

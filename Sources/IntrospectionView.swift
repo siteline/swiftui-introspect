cabf02b0f9385bd7b6f8b4b7c7928b5f893d7015
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-11 09:00:33 +0100

    WIP

--- Sources/IntrospectionView.swift
+++ Sources/IntrospectionView.swift
@@ -147,14 +147,13 @@ final class IntrospectionPlatformViewController: PlatformViewController {
         self.id = id
         super.init(nibName: nil, bundle: nil)
         self.handler = { [weak self] in
-            guard let self else {
-                return
-            }
+            guard let self else { return }
 
             // NB: .introspect makes no guarantees about the number of times its callback is invoked,
             // so the below is fair play to maximize compatibility and predictability
             handler?(self) // we call this eagerly as most customization can successfully happen without a thread hop
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
                 handler?(self) // we also thread hop to cover the rest of the cases where the underlying UI component isn't quite ready for customization
             }
         }

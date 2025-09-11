ad6feff732514c55d66e73180ed8fd30453cc8b1
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-11 11:30:45 +0100

    WIP

--- Examples/Showcase/Showcase/AppView.swift
+++ Examples/Showcase/Showcase/AppView.swift
@@ -58,64 +58,6 @@ struct ContentView: View {
     }
 }
 
-#if !os(macOS)
-struct PresentationShowcase: View {
-    @State var isSheetPresented = false
-    @State var isFullScreenPresented = false
-    @State var isPopoverPresented = false
-
-    var body: some View {
-        VStack(spacing: 20) {
-            Button("Sheet", action: { isSheetPresented = true })
-                .sheet(isPresented: $isSheetPresented) {
-                    Button("Dismiss", action: { isSheetPresented = false })
-                        #if os(iOS) || os(tvOS)
-                        .introspect(
-                            .sheet,
-                            on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .tvOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26)
-                        ) { presentationController in
-                            presentationController.containerView?.backgroundColor = .red.withAlphaComponent(0.75)
-                        }
-                        #elseif os(visionOS)
-                        .introspect(.sheet, on: .visionOS(.v1, .v2, .v26)) { sheetPresentationController in
-                            sheetPresentationController.containerView?.backgroundColor = .red.withAlphaComponent(0.75)
-                        }
-                        #endif
-                }
-
-            if #available(iOS 14, tvOS 14, *) {
-                Button("Full Screen Cover", action: { isFullScreenPresented = true })
-                    .fullScreenCover(isPresented: $isFullScreenPresented) {
-                        Button("Dismiss", action: { isFullScreenPresented = false })
-                            #if os(iOS) || os(tvOS) || os(visionOS)
-                            .introspect(
-                                .fullScreenCover,
-                                on: .iOS(.v14, .v15, .v16, .v17, .v18, .v26), .tvOS(.v14, .v15, .v16, .v17, .v18, .v26), .visionOS(.v1, .v2, .v26)
-                            ) { presentationController in
-                                presentationController.containerView?.backgroundColor = .red.withAlphaComponent(0.75)
-                            }
-                            #endif
-                    }
-            }
-
-            #if os(iOS) || os(visionOS)
-            Button("Popover", action: { isPopoverPresented = true })
-                .popover(isPresented: $isPopoverPresented) {
-                    Button("Dismiss", action: { isPopoverPresented = false })
-                        .padding()
-                        .introspect(
-                            .popover,
-                            on: .iOS(.v13, .v14, .v15, .v16, .v17, .v18, .v26), .visionOS(.v1, .v2, .v26)
-                        ) { presentationController in
-                            presentationController.containerView?.backgroundColor = .red.withAlphaComponent(0.75)
-                        }
-                }
-            #endif
-        }
-    }
-}
-#endif
-
 struct UIViewRepresentableShowcase: View {
     var body: some View {
         VStack(spacing: 10) {

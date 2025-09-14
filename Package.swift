b03e47d1077e3eb72042a36bc5dc66dee8746e55
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-15 00:14:28 +0100

    WIP

--- Package.swift
+++ Package.swift
@@ -3,29 +3,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftui-introspect",
-    platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
-        .macOS(.v10_15),
-    ],
-    products: [
-        .library(name: "SwiftUIIntrospect", targets: ["SwiftUIIntrospect"]),
-        .library(name: "SwiftUIIntrospect-Static", type: .static, targets: ["SwiftUIIntrospect"]),
-        .library(name: "SwiftUIIntrospect-Dynamic", type: .dynamic, targets: ["SwiftUIIntrospect"]),
-    ],
-    targets: [
-        .target(
-            name: "SwiftUIIntrospect",
-            path: "Sources"
-        ),
-    ]
+	name: "swiftui-introspect",
+	platforms: [
+		.iOS(.v13),
+		.tvOS(.v13),
+		.macOS(.v10_15),
+	],
+	products: [
+		.library(name: "SwiftUIIntrospect", targets: ["SwiftUIIntrospect"]),
+		.library(name: "SwiftUIIntrospect-Static", type: .static, targets: ["SwiftUIIntrospect"]),
+		.library(name: "SwiftUIIntrospect-Dynamic", type: .dynamic, targets: ["SwiftUIIntrospect"]),
+	],
+	targets: [
+		.target(
+			name: "SwiftUIIntrospect",
+			path: "Sources"
+		),
+	]
 )
 
 for target in package.targets {
-    target.swiftSettings = target.swiftSettings ?? []
-    target.swiftSettings? += [
-        .enableUpcomingFeature("ExistentialAny"),
-        .enableUpcomingFeature("InternalImportsByDefault"),
-    ]
+	target.swiftSettings = target.swiftSettings ?? []
+	target.swiftSettings? += [
+		.enableUpcomingFeature("ExistentialAny"),
+		.enableUpcomingFeature("InternalImportsByDefault"),
+	]
 }

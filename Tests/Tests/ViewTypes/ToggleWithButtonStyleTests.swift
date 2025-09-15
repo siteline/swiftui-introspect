6362dfff553334644796aff0bc68ace6a510a20d
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-15 21:59:58 +0100

    WIP

--- Tests/Tests/ViewTypes/ToggleWithButtonStyleTests.swift
+++ Tests/Tests/ViewTypes/ToggleWithButtonStyleTests.swift
@@ -6,38 +6,28 @@ import Testing
 @MainActor
 @Suite
 struct ToggleWithButtonStyleTests {
-	#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 	typealias PlatformToggleWithButtonStyle = NSButton
-	#endif
 
-	@available(macOS 12, *)
+	@available(macOS, introduced: 12, obsoleted: 26)
 	@Test func introspect() async throws {
 		let (entity1, entity2, entity3) = try await introspection(of: PlatformToggleWithButtonStyle.self) { spy1, spy2, spy3 in
 			VStack {
 				Toggle("", isOn: .constant(true))
 					.toggleStyle(.button)
-					#if os(macOS)
-					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15, .v26), customize: spy1)
-					#endif
-
+					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15), customize: spy1)
+				
 				Toggle("", isOn: .constant(false))
 					.toggleStyle(.button)
-					#if os(macOS)
-					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15, .v26), customize: spy2)
-					#endif
-
+					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15), customize: spy2)
+				
 				Toggle("", isOn: .constant(true))
 					.toggleStyle(.button)
-					#if os(macOS)
-					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15, .v26), customize: spy3)
-					#endif
+					.introspect(.toggle(style: .button), on: .macOS(.v12, .v13, .v14, .v15), customize: spy3)
 			}
 		}
-		#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 		#expect(entity1.state == .on)
 		#expect(entity2.state == .off)
 		#expect(entity3.state == .on)
-		#endif
 	}
 }
 #endif

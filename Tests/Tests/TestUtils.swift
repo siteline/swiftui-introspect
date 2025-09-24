4b19344c6eb88b90692657db9d50c9583605464d
David Roman <2538074+davdroman@users.noreply.github.com>
2025-09-24 14:31:49 +0100

    WIP

--- Tests/Tests/TestUtils.swift
+++ Tests/Tests/TestUtils.swift
@@ -10,7 +10,7 @@ enum TestUtils {
 	static let window = UIWindow(frame: UIScreen.main.bounds)
 	#endif
 
-	static func present(view: some View, file: StaticString = #file, line: UInt = #line) async {
+	static func present(view: some View, file: StaticString = #file, line: UInt = #line) {
 		if let window =
 			UIApplication.shared.connectedScenes.compactMap({ $0 as? UIWindowScene }).first?.windows.first
 			??
@@ -18,9 +18,6 @@ enum TestUtils {
 		{
 			window.rootViewController = UIHostingController(rootView: view)
 		} else {
-			while UIApplication.shared.delegate == nil {
-				await Task.yield()
-			}
 			window.rootViewController = UIHostingController(rootView: view)
 			window.makeKeyAndVisible()
 			window.layoutIfNeeded()
@@ -64,7 +61,7 @@ func introspection<Entity: AnyObject & Sendable>(
 			},
 		)
 
-		await TestUtils.present(view: view)
+		TestUtils.present(view: view)
 
 		let startInstant = Date()
 		while
@@ -104,7 +101,7 @@ func introspection<Entity: AnyObject & Sendable>(
 				},
 			)
 
-			await TestUtils.present(view: view)
+			TestUtils.present(view: view)
 
 			let startInstant = Date()
 			while
@@ -156,7 +153,7 @@ func introspection<Entity: AnyObject & Sendable>(
 					},
 				)
 
-				await TestUtils.present(view: view)
+				TestUtils.present(view: view)
 
 				let startInstant = Date()
 				while
@@ -218,7 +215,7 @@ func introspection<Entity: AnyObject & Sendable>(
 						},
 					)
 
-					await TestUtils.present(view: view)
+					TestUtils.present(view: view)
 
 					let startInstant = Date()
 					while

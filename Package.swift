70264dcb1022a5033649876d92d5b723d8bdbc5b
David Roman <2538074+davdroman@users.noreply.github.com>
2026-06-09 14:11:44 +0100

    enable upcoming features

--- Package.swift
+++ Package.swift
@@ -26,6 +26,10 @@ for target in package.targets {
 	target.swiftSettings = target.swiftSettings ?? []
 	target.swiftSettings? += [
 		.enableUpcomingFeature("ExistentialAny"),
+		.enableUpcomingFeature("ImmutableWeakCaptures"),
+		.enableUpcomingFeature("InferIsolatedConformances"),
 		.enableUpcomingFeature("InternalImportsByDefault"),
+		.enableUpcomingFeature("MemberImportVisibility"),
+		.enableUpcomingFeature("NonisolatedNonsendingByDefault"),
 	]
 }

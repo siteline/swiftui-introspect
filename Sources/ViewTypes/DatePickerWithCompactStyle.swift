8dda8b6fb1f5d1422297f66224059528a3fa47d4
David Roman <2538074+davdroman@users.noreply.github.com>
2026-08-28 01:13:16 +0100

    fix: correct unavailable compact date picker versions

--- Sources/ViewTypes/DatePickerWithCompactStyle.swift
+++ Sources/ViewTypes/DatePickerWithCompactStyle.swift
@@ -68,7 +68,7 @@ extension IntrospectableViewType where Self == DatePickerWithCompactStyleType {
 #if canImport(UIKit)
 extension iOSViewVersion<DatePickerWithCompactStyleType, UIDatePicker> {
     @available(*, unavailable, message: ".datePickerStyle(.compact) isn't available on iOS 13")
-    public static let v13 = Self(for: .v13)
+    public static let v13 = Self.unavailable()
     public static let v14 = Self(for: .v14)
     public static let v15 = Self(for: .v15)
     public static let v16 = Self(for: .v16)
@@ -83,7 +83,7 @@ extension visionOSViewVersion<DatePickerWithCompactStyleType, UIDatePicker> {
 #elseif canImport(AppKit) && !targetEnvironment(macCatalyst)
 extension macOSViewVersion<DatePickerWithCompactStyleType, NSDatePicker> {
     @available(*, unavailable, message: ".datePickerStyle(.compact) isn't available on macOS 10.15")
-    public static let v10_15 = Self(for: .v10_15)
+    public static let v10_15 = Self.unavailable()
     public static let v10_15_4 = Self(for: .v10_15_4)
     public static let v11 = Self(for: .v11)
     public static let v12 = Self(for: .v12)
